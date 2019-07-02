23b3126372d6b2165c51fe55d146dbbc8c783196
Suraches Seesat <suraches.see@ascendcorp.com>
2019-07-02 17:16:24 +0700

    update

--- express/server.js
+++ express/server.js
@@ -1,5 +1,7 @@
 const express = require('express')
+var bodyParser = require("body-parser")
 const app = express()
+app.use(bodyParser.json());
 const books = require('./db')
 
 app.get('/', (req, res) => {
@@ -12,6 +14,17 @@ app.get('/books', (req, res) => {
 
 app.get('/books/:id', (req, res) => {
   res.json(books.find(book => book.id === req.params.id))
+})
+
+app.put('/books/:id', (req, res) => {
+	const updateIndex = books.findIndex(book => book.id === req.params.id)
+	res.json(Object.assign(books[updateIndex], req.body))
+})
+
+app.delete('/books/:id', (req, res) => {
+	const deletedIndex = books.findIndex(book => book.id === req.params.id)
+	books.splice(deletedIndex, 1)
+	res.status(204).send()
 })
 
 app.listen(3000, () => {
