44f01517179d54df02411028631607fe83db9d85
suraches <suraches.see@ascendcorp.com>
2019-07-10 01:05:27 +0700

    update routes

--- labs/start/routes.js
+++ labs/start/routes.js
@@ -21,40 +21,37 @@ Route.get('/', ({ request, response }) => {
   })
 })
 
-// Customers
-Route.get('api/v1/customers', 'CustomerController.index').middleware('auth')
-Route.get('api/v1/customers/:id', 'CustomerController.show').middleware([
-  'findCustomer'
-])
-Route.post('api/v1/customers', 'CustomerController.store')
-Route.patch('api/v1/customers/:id', 'CustomerController.update').middleware([
-  'findCustomer'
-])
-Route.delete('api/v1/customers/:id', 'CustomerController.delete').middleware([
-  'findCustomer'
-])
-
-// projects
-Route.get('api/v1/projects', 'ProjectController.index')
-Route.get('api/v1/projects/:id', 'ProjectController.show').middleware(['findProject'])
-Route.post('api/v1/projects', 'ProjectController.store')
-Route.patch('api/v1/projects/:id', 'ProjectController.update').middleware([
-  'findProject'
-])
-Route.delete('api/v1/projects/:id', 'ProjectController.delete').middleware([
-  'findProject'
-])
-
-// tasks
-Route.get('api/v1/tasks', 'TaskController.index')
-Route.get('api/v1/tasks/:id', 'TaskController.show').middleware(['findTask'])
-Route.post('api/v1/tasks', 'TaskController.store')
-Route.patch('api/v1/tasks/:id', 'TaskController.update').middleware(['findTask'])
-Route.delete('api/v1/tasks/:id', 'TaskController.delete').middleware(['findTask'])
-
-// Users
-Route.get('api/v1/users', 'UserController.index').middleware('auth')
-
 // Auth
-Route.post('/auth/register', 'AuthController.register')
-Route.post('/auth/login', 'AuthController.login')
+Route.group(() => {
+  Route.post('register', 'AuthController.register')
+  Route.post('login', 'AuthController.login')
+}).prefix('auth')
+
+Route.group(() => {
+
+  // Customers
+  Route.get('customers', 'CustomerController.index')
+  Route.post('customers', 'CustomerController.store')
+  Route.get('customers/:id', 'CustomerController.show').middleware('findCustomer')
+  Route.patch('customers/:id', 'CustomerController.update').middleware('findCustomer')
+  Route.delete('customers/:id', 'CustomerController.delete').middleware('findCustomer')
+
+
+  // Projects
+  Route.get('projects', 'ProjectController.index')
+  Route.post('projects', 'ProjectController.store')
+  Route.get('projects/:id', 'ProjectController.show').middleware('findProject')
+  Route.patch('projects/:id', 'ProjectController.update').middleware('findProject')
+  Route.delete('projects/:id', 'ProjectController.delete').middleware('findProject')
+
+  // tasks
+  Route.get('tasks', 'TaskController.index')
+  Route.post('tasks', 'TaskController.store')
+  Route.get('tasks/:id', 'TaskController.show').middleware('findTask')
+  Route.patch('tasks/:id', 'TaskController.update').middleware('findTask')
+  Route.delete('tasks/:id', 'TaskController.delete').middleware('findTask')
+
+  // Users
+  Route.get('users', 'UserController.index')
+
+}).prefix('api/v1').middleware('auth')
